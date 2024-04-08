f229c2ed149db58961d0c62dba2fcf052dd2f3e1
ackimixs <ackimixs@protonmail.com>
2024-04-08 15:49:05 +0200

    opencv 4.6

--- createCharuco.cpp
+++ createCharuco.cpp
@@ -1,8 +1,8 @@
-#include <opencv2/aruco.hpp>
+#include <opencv2/charuco.hpp>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char** argv) {
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     cv::Ptr<cv::aruco::CharucoBoard> board = cv::aruco::CharucoBoard::create(5, 7, 0.04f, 0.02f, dictionary);
     cv::Mat boardImage;
     board->draw(cv::Size(600, 500), boardImage, 10, 1);
