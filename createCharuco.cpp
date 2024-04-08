2dfcde4417359e49b69b9c98e839b016fc0140fb
ackimixs <ackimixs@protonmail.com>
2024-04-08 15:44:48 +0200

    opencv 4.6

--- createCharuco.cpp
+++ createCharuco.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/aruco.hpp>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char** argv) {
     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
