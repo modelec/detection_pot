d5d1808a95ad57157c1c3eab48b0306fb5244984
ackimixs <ackimixs@protonmail.com>
2024-05-02 16:43:55 +0200

    calibration

--- calibrationCharuco.cpp
+++ calibrationCharuco.cpp
@@ -44,6 +44,7 @@ the use of this software, even if advised of the possibility of such damage.
 #include <vector>
 #include <iostream>
 #include "aruco_samples_utility.hpp"
+#include <lccv.hpp>
 
 using namespace std;
 using namespace cv;
