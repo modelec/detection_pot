63c1d850f438a735b7d0b75fd74a9b42a9bff04f
ackimixs <ackimixs@protonmail.com>
2024-05-02 19:32:31 +0200

    calib

--- calibrationCharuco.cpp
+++ calibrationCharuco.cpp
@@ -11,8 +11,8 @@ int main(int argc, char *argv[]) {
 
     int squaresX = 5;
     int squaresY = 7;
-    float squareLength = 0.04f;
-    float markerLength = 0.02f;
+    float squareLength = 0.041f;
+    float markerLength = 0.0205f;
     std::string outputFile = "camera_calibration.yml";
 
     // create charuco board object
